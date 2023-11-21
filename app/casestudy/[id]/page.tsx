ab27ae408208b42c8428d040ddb7cbb8a284b454
nehal7-ml <nehal.sk.99@gmail.com>
2023-11-21 18:38:43 +0530

    created all section for case study

--- app/casestudy/[id]/page.tsx
+++ app/casestudy/[id]/page.tsx
@@ -1,3 +1,4 @@
+import UserPersonaCard from "@/components/casestudies/UserPersonaCard";
 import Image from "next/image";
 
 
@@ -8,29 +9,68 @@ const caseStudy = {
     problemStatement: "Lorem ipsum e et dolore magna aliqua. Ac orci pellentesque eu tincidunt. Tempus quam pellentesque ns.",
     userProblems: ["problem 1", "problem 2"],
     possibleSolutions: ["solution 1", "solution 2", "solution 3"],
-    userResearch: "lorem ipsum",
     userPersonas: [
         {
-            bio: "user bio",
+            bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Tempus quam pellentesque nec nam aliquam. Ac turpis egestas integer eget aliquet nibh. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque fermentum dui faucibus in ornare quam. Proin sed libero enim sed. Fringilla ut morbi tincidunt augue interdum velit. Et ligula ullamcorper malesuada proin libero. Sagittis eu volutpat odio facilisis. Aliquam id diam maecenas ultricies mi. Elit eget gravida cum sociis. Interdum varius sit amet mattis vulputate enim nulla aliquet. Integer vitae justo eget magna fermentum iaculis eu non. Urna neque viverra justo nec ultrices. Odio eu feugiat pretium nibh ipsum. Volutpat odio facilisis mauris sit amet. Arcu ac tortor dignissim convallis. Integer enim neque volutpat ac tincidunt vitae semper quis.",
             name: "user anme",
             gender: "male",
-            age: "30",
+            age: 30,
             goals: ["goal1", "goal2", "goal3", "goal4"],
             painPoints: ["ponit1", "ponit2", "ponit3", "ponit4"],
             image: {
-                src: 'https://picsum.photos/900',
+                id: "91827302381-2",
+                src: 'https://picsum.photos/900?random=1',
                 name: "pic name"
             }
 
         }
     ],
-    image: {
-        src: 'https://picsum.photos/900',
+    image1: {
+        src: '/casestudy-1.png',
+        name: "pic name"
+
+    },
+    image2: {
+        src: '/casestudy-2.png',
         name: "pic name"
 
     },
 
-    goals: []
+    goals: ["goal1", "goal2", "goal3", "goal4"],
+    keyLearning: "Amet tellus cras adipiscing enim eu turpis. Lobortis scelerisque fermentum dui faucibus. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. Ultricies tristique nulla aliquet enim tortor. Semper auctor neque vitae tempus quam pellentesque nec. Vivamus arcu felis bibendum ut tristique et egestas. Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Egestas dui id ornare arcu odio ut sem nulla. Risus in hendrerit gravida rutrum quisque. Volutpat consequat mauris nunc congue. Justo laoreet sit amet cursus sit amet. Amet justo donec enim diam vulputate. Arcu bibendum at varius vel pharetra vel turpis nunc eget. At augue eget arcu dictum varius duis. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. A arcu cursus vitae congue mauris rhoncus aenean vel elit.",
+    userResearch: "Amet tellus cras adipiscing enim eu turpis. Lobortis scelerisque fermentum dui faucibus. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. Ultricies tristique nulla aliquet enim tortor. Semper auctor neque vitae tempus quam pellentesque nec. Vivamus arcu felis bibendum ut tristique et egestas. Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Egestas dui id ornare arcu odio ut sem nulla. Risus in hendrerit gravida rutrum quisque. Volutpat consequat mauris nunc congue. Justo laoreet sit amet cursus sit amet. Amet justo donec enim diam vulputate. Arcu bibendum at varius vel pharetra vel turpis nunc eget. At augue eget arcu dictum varius duis. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. A arcu cursus vitae congue mauris rhoncus aenean vel elit.",
+    uniqueFeatures: "Amet tellus cras adipiscing enim eu turpis. Lobortis scelerisque fermentum dui faucibus. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. Ultricies tristique nulla aliquet enim tortor. Semper auctor neque vitae tempus quam pellentesque nec. Vivamus arcu felis bibendum ut tristique et egestas. Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Egestas dui id ornare arcu odio ut sem nulla. Risus in hendrerit gravida rutrum quisque. Volutpat consequat mauris nunc congue. Justo laoreet sit amet cursus sit amet. Amet justo donec enim diam vulputate. Arcu bibendum at varius vel pharetra vel turpis nunc eget. At augue eget arcu dictum varius duis. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. A arcu cursus vitae congue mauris rhoncus aenean vel elit.",
+    wireFrame: [{
+        src: 'https://picsum.photos/300?random=1',
+        name: "pic name"
+    }],
+    architecture: {
+        src: 'https://picsum.photos/300?random=3',
+        name: "pic name"
+    },
+    userFlow: {
+        src: 'https://picsum.photos/300?random=10',
+        name: "pic name"
+    },
+    hifi: [{
+        src: 'https://picsum.photos/300?random=5',
+        name: "pic name"
+    },
+    {
+        src: 'https://picsum.photos/300?random=5',
+        name: "pic name"
+    },
+    {
+        src: 'https://picsum.photos/300?random=5',
+        name: "pic name"
+    }
+
+
+
+    ]
+
+
+
 }
 function CaseStudy() {
     return (
@@ -54,16 +94,16 @@ function CaseStudy() {
 
                 </div>
                 <div className="flex justify-center items-center my-5 rounded-lg overflow-hidden">
-                    <Image className="rounded-lg" src={caseStudy.image.src} alt="cse-study" height={500} width={500} />
+                    {caseStudy.image1 ? <Image className=" rounded-lg object-contain" src={caseStudy.image1.src} alt={'casestudy-1'} height={500} width={500} /> :
+                        <Image className="l rounded-lg object-contain" src={'/casestudy-1.png'} alt={'casestudy-1'} height={500} width={500} />
+                    }
                 </div>
 
                 {/* Section 3: Project Goals */}
                 <section className="relative dark:bg-zinc-900 rounded-2xl shadow-lg p-5 mb-8 z-50">
                     <h2 className="text-2xl font-bold text-center text-[#7850CD]">Project Goals</h2>
                     <ul className="flex gap-5 justify-center items-center mt-4">
-                        <li>Goal 1</li>
-                        <li>Goal 2</li>
-                        <li>Goal 3</li>
+                        {caseStudy.goals.map((goal, index) => <li key={index}>{goal}</li>)}
                     </ul>
                 </section>
 
@@ -95,7 +135,7 @@ function CaseStudy() {
                 </section>
 
 
-                <section className="relative">
+                <section className="relative mb-8">
                     <div className="relative dark:bg-zinc-900 rounded-2xl shadow-lg p-5 mb-8 z-50">
                         <h2 className="text-2xl font-bold text-[#7850CD] mb-10">Understanding the Problem</h2>
                         <p>{caseStudy.userResearch}</p>
@@ -120,6 +160,78 @@ function CaseStudy() {
                     <Image className="absolute -z-10 -translate-x-full  bottom-0 translate-y-10" src={'/case-study-icon-1.png'} alt="icon-1" height={130} width={75} />
 
                 </div>
+
+                <div className="flex justify-center items-center my-5 rounded-lg overflow-hidden">
+                    {caseStudy.image2 ? <Image className=" rounded-lg object-contain" src={caseStudy.image2.src} alt={'casestudy-1'} height={500} width={500} /> :
+                        <Image className="l rounded-lg object-contain" src={'/casestudy-2.png'} alt={'casestudy-1'} height={500} width={500} />
+                    }
+                </div>
+
+                <section className="my-10">
+                    <h1 className="text-4xl font-bold text-[#7850CD]">User Personas</h1>
+                    {caseStudy.userPersonas.map((persona, index) => {
+
+                        return <div key={index}>
+                            <UserPersonaCard
+                                {...persona}
+
+                            />
+                        </div>
+                    })}
+                </section>
+                <section className="mb-8 ">
+                    <h2 className="text-[#7850CD]">User Research</h2>
+                    <p>{caseStudy.userResearch}</p>
+                </section>
+                <section className="mb-8 ">
+                    <h2 className="text-[#7850CD]">Competetive Analysis </h2>
+                </section>
+                <section className="mb-8 ">
+                    <h2 className="text-[#7850CD]">Unique features</h2>
+                    <p>{caseStudy.uniqueFeatures}</p>
+                </section>
+
+                <section className="mb-8 ">
+                    {caseStudy.wireFrame && <>
+                        <h2 className="text-[#7850CD]">Wireframes</h2>
+                        <div className="flex gap-2 flex-wrap max-w-full">
+                            {caseStudy.wireFrame.map((wireframe) => <Image src={wireframe.src} alt={"architecture"} height={500} width={500} />)}</div>
+
+                    </>}
+                </section>
+                <section className="mb-8 ">
+                    {caseStudy.architecture &&
+
+                        <><div className="rounded-lg shadow-lg p-10 text-center ">
+                            <h2 className="text-[#7850CD]">Information Architecture </h2>
+                            <p className="text-left font-semibold">To get innovative solutions into the hands of your users, I needed to concentrate on idea generation.  Based on my understanding of the problem, I brainstormed the Information architecture and user flow to ensure smooth experience for the users. </p>
+                        </div>
+                            <Image src={caseStudy.architecture.src} alt={"architecture"} height={500} width={500} />
+
+                        </>}
+                </section>
+                <section className="mb-8 ">
+                    {caseStudy.hifi && <>
+                        <h2 className="text-[#7850CD]">Hi-Fi Designs</h2>
+
+                        <div className="flex gap-2 flex-wrap max-w-full">
+                            {caseStudy.hifi.map((image, index) => <div key={index} className="flex flex-col justify-center items-center" >
+                                <Image src={image.src} alt={image.name} height={500} width={500} />
+                                <div className="font-bold"># {image.name}</div>
+                            </div>)}
+                        </div>
+                    </>}
+                </section>
+
+                <section className="relative mb-8 bg-[radial-gradient(#744ACC,#744ACCE5)] flex flex-col justify-center items-center overflow-hidden">
+                    <Image className="object-contain" src={'/casestudy-3.png'} alt="casestudy-3" height={300} width={300} />
+                    <div className="absolute top-0 left-0 -translate-x-1/2 -translate-y-2/3 aspect-square h-full rounded-full  bg-gradient-to-b from-[#7850CD00] to-[#9D6FFF] z-0"></div>
+                    <div className="absolute bottom-0 right-0 translate-x-1/3 translate-y-2/3 aspect-square h-full rounded-full  bg-gradient-to-t from-[#7850CD00] to-[#9D6FFF] z-0"></div>
+                    <div className="text-white p-10 z-10">
+                        <h2 className="text-3xl">Key Learnings</h2>
+                        <p className="text-lg">{caseStudy.keyLearning}</p>
+                    </div>
+                </section>
             </div>
 
 
---
+++ components/casestudies/UserPersonaCard.tsx
@@ -0,0 +1,47 @@
+import { Image as userImage } from "@prisma/client";
+import Image from "next/image";
+
+export type UserPersonaProps = {
+    name: string,
+    age: number,
+    gender: string,
+    bio: string,
+    goals: string[],
+    painPoints: string[],
+    image: userImage
+}
+
+function UserPersonaCard({ name, age, bio, gender, goals, image, painPoints }: UserPersonaProps) {
+    return (<>
+        <div className="flex flex-col lg:flex-row rounded-2xl shadow-lg p-4">
+            <div className="relative lg:w-1/2 text-white">
+                <Image className="w-full h-full rounded-lg" src={image.src} alt={name} height={500} width={500} />
+                <div className="absolute bottom-0 w-full flex flex-col justify-end items-center">
+                    <p className="text-xl font-bold">Name: {name}</p>
+                    <p className="text-xl font-bold"><strong>Age:</strong>Age: {age}</p>
+                    <p className="text-xl font-bold"><strong>Gender:</strong>Gender: {gender}</p>
+                </div>
+            </div>
+            <div className="lg:w-1/2 p-4">
+                <h2 className=" text-[#7850CD] font-bold mt-4 mb-2">Bio</h2>
+                <p>{bio}</p>
+                <div className="flex lg:flex-row justify-center">
+                    <div className="w-1/2">
+                        <h3 className="text-[#7850CD]">Pain Points</h3>
+                        <div className="list-disc">{painPoints.map((point, index) => <li key={index}>{point}</li>)}</div>
+                    </div>
+                    <div className="w-1/2">
+                        <h3 className="text-[#7850CD]">Goals</h3>
+                        <div className="list-disc">{goals.map((goal, index) => <li key={index}>{goal}</li>)}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>);
+}
+
+
+
+
+
+export default UserPersonaCard;
