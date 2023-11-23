9393a7f3c836b66395bdf0cb6e054ccc1d698c35
nehal7-ml <nehal.sk.99@gmail.com>
2023-11-24 01:29:14 +0530

    added break work for responsive blogs

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -69,7 +69,7 @@ export default function NavBar({ session, darkMode }: { session: Session | null,
           </Link>
 
 
-          <div className={` ${!mobileMenuOpen ? 'hidden' : 'flex fixed right-0 h-screen w-screen pt-10 animate-slide-left-fade text-center z-[100] bg-white dark:bg-black'}   gap-10  top-full h-screen w-full flex-col items-center justify-start lg:pt-0 lg:static lg:flex lg:flex-row lg:gap-5 lg:justify-center lg:h-full lg:bg-inherit lg:w-fit`}>
+          <div className={` ${!mobileMenuOpen ? 'hidden' : 'flex absolute right-0 top-full h-screen w-screen pt-10 animate-slide-left-fade text-center z-[100] bg-white dark:bg-black'}   gap-10   h-screen w-full flex-col items-center justify-start lg:pt-0 lg:static lg:flex lg:flex-row lg:gap-5 lg:justify-center lg:h-full lg:bg-inherit lg:w-fit`}>
 
             <div className="relative group">
               <button
@@ -144,7 +144,7 @@ export default function NavBar({ session, darkMode }: { session: Session | null,
               <input
                 type="text"
                 placeholder="Search"
-                className="dark:bg-[#272F43] text-white rounded-md py-1 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500 w-32 lg:w-auto"
+                className="dark:bg-[#272F43] text-white rounded-md py-1 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500 w-32 lg:w-38"
               />
               <button className="absolute right-2 top-1/2 transform -translate-y-1/2">
               </button>
@@ -200,8 +200,8 @@ function ToggleDarkMode({ enabled, className }: { enabled: boolean, className?: 
   }, [isToggled]);
 
   return (
-    <div className={"container flex justify-center items-center " + className}>
-      <label className="flex items-center cursor-pointer w-6 h-6">
+    <div className={"container flex justify-center items-center  lg:mx-auto lg:w-12 " + className}>
+      <label className="flex items-center cursor-pointer">
         <div className="relative">
           <input
             type="checkbox"
