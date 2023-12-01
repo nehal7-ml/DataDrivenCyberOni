bdf22bc38f9ab0817fbfb7375b36270a2da10eda
nehal7-ml <nehal.sk.99@gmail.com>
2023-12-01 18:07:47 +0530

    fix blog zindex

--- components/blogs/BlogContent.tsx
+++ components/blogs/BlogContent.tsx
@@ -11,33 +11,35 @@ function BlogContent({ content }: { content: string }) {
     const [resizeScript, setResizeScript] = useState('')
     const iframe = useRef<HTMLIFrameElement>(null)
     function resizeIframe() {
-        
+
     }
 
     useEffect(() => {
         setResizeScript(`<script id="resizeScript" >
         window.document.children[0].addEventListener('resize',()=> {
             console.log("html resizeiong");
-            window.parent.postMessage({size: window.document.getElementsByTagName('html')[0].scrollHeight}, "${window.origin}");
+            window.parent.postMessage({ type:"resize",size: window.document.getElementsByTagName('html')[0].scrollHeight}, "${window.origin}");
         })
         console.log("loading");
-        window.parent.postMessage({size: window.document.getElementsByTagName('html')[0].scrollHeight}, "${window.origin}");
+        window.parent.postMessage({ type:"resize", size: window.document.getElementsByTagName('html')[0].scrollHeight}, "${window.origin}");
+
+
         </script>`)
 
         window.addEventListener("message", (event) => {
 
             console.log("loaded window", event.data);
-            if (iframe.current) iframe.current.style.height =event.data.size.toString()+"px";
+            if (iframe.current && event.data.type==="resize") iframe.current.style.height = event.data.size.toString() + "px";
         });
-        
-    }, []);
+
+    }, []); 
 
 
 
     // console.log(content);
     return (<>
 
-        {<iframe ref={iframe} onLoad={resizeIframe} className="w-full h-fit overflow-y-auto" src={getBlobURL(content + resizeScript, "text/html;")}></iframe>}
+        {<iframe ref={iframe} onLoad={resizeIframe} className="w-full h-fit overflow-y-auto z-50" src={getBlobURL(content + resizeScript, "text/html;")}></iframe>}
     </>);
 }
 
