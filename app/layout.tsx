b333590c6ff8e6163c1a02523bd12a917f548e45
01100111011011000110100 1011101000110001101101000 <tyrique1.daniel@gmail.com>
2023-12-07 13:24:15 -0700

    google tag assistant test

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import { authOptions } from "./api/auth/[...nextauth]/route";
 import { abel, inter, nunito, sfPro } from "./fonts";
 import "./globals.css";
 import { Facebook } from 'lucide-react';
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: Owner.seo.metaTitle,
@@ -59,7 +60,30 @@ export default async function RootLayout({
     <html lang="en">
       <Head>
         <link rel="canonical" href="https://www.cybershoptech.com" />
+        <title>Your Page Title</title>
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-55E14FBFE1`}
+      />
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-55E14FBFE1');
+        `}
+      </Script>
+     
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=G-55E14FBFE1"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
       <body
         className={`${cx(
           sfPro.variable,
@@ -67,7 +91,7 @@ export default async function RootLayout({
           abel.variable,
           nunito.variable,
         )} 
-                        ${theme?.value} antialiased overflow-x-hidden`}
+                        ${theme?.value} overflow-x-hidden antialiased`}
       >
         <div className="fixed h-full w-screen dark:bg-gray-900 dark:text-white " />
         <Suspense fallback="...">
@@ -76,7 +100,7 @@ export default async function RootLayout({
             darkMode={theme?.value === "dark" ? true : false}
           />
         </Suspense>
-        <main className="relative min-h-screen w-full  pt-24 dark:bg-gray-900 dark:text-white overflow-x-hidden">
+        <main className="relative min-h-screen w-full  overflow-x-hidden pt-24 dark:bg-gray-900 dark:text-white">
           {children}
         </main>
         {
