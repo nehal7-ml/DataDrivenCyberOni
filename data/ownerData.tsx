5b1be41a3938c69bd8ea57af1900ba1962093582
01100111011011000110100 1011101000110001101101000 <tyrique1.daniel@gmail.com>
2023-12-11 12:35:46 -0700

    added privacy policy and terms

--- data/ownerData.tsx
+++ data/ownerData.tsx
@@ -35,3 +35,115 @@ export const Owner = {
     ],
   },
 };
+
+export const Site = {
+  terms_conditions: `Introduction and Purpose:
+
+Welcome to Cyberoni! We are committed to providing you with exceptional services and products. Before you proceed, please take a moment to read and understand our terms and conditions. By using our website or purchasing our products, you agree to abide by these terms.
+
+Intellectual Property Protection:
+
+Ownership: All content on this website, including text, graphics, logos, images, and software, is the property of Cyberoni and is protected by intellectual property laws.
+
+Usage Restrictions: You may not copy, modify, distribute, or create derivative works without our explicit consent. Any unauthorized use is strictly prohibited to avoid ambiguity.
+
+Limitation of Liability:
+
+Scope: To the maximum extent allowed by law, Cyberoni's liability is limited.
+
+Examples: For instance, in no event will our liability exceed the amount you paid for the product or service.
+
+Data Privacy and Security:
+
+Data Collection and Use: We take your data privacy seriously. We collect data for SEO audits and penetration testing, but rest assured, your data will not be shared with third parties without your explicit consent. We use it solely to improve our services.
+User Conduct and Responsibilities:
+
+Acceptable Usage: To maintain a safe environment for all users, please refrain from any actions that may disrupt the website or interfere with others' access. Review our guidelines for a clear understanding of acceptable behavior.
+Warranty Disclaimers:
+
+Nature of Warranties: We provide products and services 'as is' and 'as available.' We do not make any warranties, express or implied, about our website, products, or services. This includes but is not limited to fitness for a particular purpose or error-free use.
+Terms Modification Process:
+
+Modifications: We may update these terms as needed. Changes will be communicated through updates on our website. Your continued use after modifications indicates your acceptance.
+Governing Law and Dispute Resolution:
+
+Dispute Resolution: In case of disputes, we encourage alternative dispute resolution mechanisms like arbitration. Litigation should be the last resort. Our terms are governed by California law, and legal matters will be addressed in Santa Clara County.
+Indemnification Clause:
+
+Protection: You agree to indemnify and protect Cyberoni against any claims or expenses arising from your misuse of our services or violation of these terms.
+ Termination Rights:
+
+Termination: We reserve the right to terminate your access or use at any time. Upon termination, you must cease all use and delete our content.
+Accessibility and Inclusivity:
+
+Accessibility: If you require assistance or alternative formats due to disabilities, please contact us. We are committed to making our terms accessible to all users.
+Contact Information and Support:
+
+Support: If you have questions or need support, please contact us at support@cyberoni.com. We value your feedback and aim to improve our services.
+Shipping:
+
+Prompt Shipping: We make every effort to ship our products promptly after receiving your order.
+
+Shipping Delays: However, we are not responsible for any delays or damages caused by shipping carriers.
+
+Shipping Information Accuracy: We are not liable for any losses, damages, or expenses arising from your failure to receive a shipment or your failure to provide accurate shipping information.
+
+Returns and Refunds:
+
+Customer Satisfaction: We want you to be completely satisfied with your purchase.
+
+Return Request: If you are not satisfied with a product you receive from us, please contact us within 30 days of the purchase date to request a return.
+
+Refund Process: We will provide you with instructions for returning the product. Once we receive the returned product, we will issue a refund for the purchase price, less shipping and handling fees. Refunds will be made to the original payment method.
+
+Disclaimer of Warranties:
+
+As-Is Basis: We provide our products and services on an "as is" and "as available" basis.
+
+No Warranties: We do not make any representations or warranties of any kind, express or implied, including, but not limited to, warranties of merchantability, fitness for a particular purpose, non-infringement, or course of performance.
+
+No Guarantee: We do not warrant that our website, products, or services will meet your requirements, be uninterrupted, be error-free, or be secure. We are not responsible for any viruses or other harmful components transmitted through our website or by using our products or services.
+
+Limitation of Liability:
+
+Exclusion of Damages: In no event will Cyberoni or its affiliates, directors, officers, employees, agents, suppliers, or licensors be liable to you or any third party for any direct, indirect, incidental, consequential, special, or punitive damages arising from or in connection with your use of our website, products, or services, or your inability to use our website, products, or services.
+
+Limitation of Liability: In no event will our liability to you exceed the amount you paid for the product or service that gave rise to the claim.
+
+Indemnification:
+
+Defend and Indemnify: You agree to defend, indemnify, and hold harmless Cyberoni and its affiliates, directors, officers, employees, agents, suppliers, and licensors from and against any claims, liabilities, damages, judgments, awards, losses, costs, expenses, or fees arising out of or relating to your use of our website, products, or services, your violation of these terms and conditions, or your violation of any rights of a third party.
+Entire Agreement:
+
+These terms and conditions constitute the entire agreement between you and Cyberoni with respect to your use of our website, products, or services and supersede all prior or contemporaneous communications and proposals, whether oral or written, between you and Cyberoni.
+
+If you have any questions about these terms and conditions or our products or services, please contact us at support@cyberoni.com. We appreciate your business and look forward to serving you.
+
+Changes to Terms and Conditions:
+
+We reserve the right to modify these terms and conditions at any time without notice. Any changes we make to these terms and conditions will be effective immediately upon posting on our website. Your continued use of our website, products, or services after we have made changes to these terms and conditions constitutes your acceptance of the revised terms and conditions.
+
+Governing Law and Jurisdiction:
+
+These terms and conditions shall be governed by and construed in accordance with the laws of the State of California, without regard to its conflicts of law principles. Any legal action or proceeding arising out of or relating to these terms and conditions shall be brought in the state or federal courts located in the County of Santa Clara, California.
+
+Severability:
+
+If any provision of these terms and conditions is found to be unlawful, void, or unenforceable, that provision shall be deemed severable from the remaining provisions and shall not affect their validity and enforceability.
+
+Waiver:
+
+The failure of Cyberoni to enforce any right or provision of these terms and conditions will not be considered a waiver of such right or provision. The waiver of any such right or provision will be effective only if in writing and signed by a duly authorized representative of Cyberoni.
+
+Assignment:
+
+You may not assign, transfer, or sublicense any or all of your rights or obligations under these terms and conditions without our express prior written consent. Cyberoni may assign, transfer, or delegate any or all of its rights and obligations under these terms and conditions without notice to you.
+
+Contact Information:
+
+If you have any questions or concerns about these terms and conditions or our products and services, please feel free to contact us at support@cyberoni.com. We value your feedback and strive to provide excellent customer service.
+
+Thank you for choosing Cyberoni for your software development needs. We are dedicated to delivering high-quality services and products to our valued customers. By using our website or purchasing our products, you acknowledge that you have read, understood, and agreed to be bound by these terms and conditions.
+`,
+  privacyPolicy: "",
+};
