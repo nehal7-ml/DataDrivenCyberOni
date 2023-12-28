8f0d6c8a059d87d1f0d68193e496ec3a953c9e6a
nehal7-ml <nehal.sk.99@gmail.com>
2023-12-29 05:17:31 +0530

    added utm props to redirected links

---
+++ app/referrals/[prefix]/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import apiHandler from "@/errorHandler";
+import { addClick, update } from "@/crud/referral";
+import { ReferralType } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { prisma } from "@/prisma/prismaClient";
+
+
+export default async function Referral({ params }: { params: { prefix: string } }) {
+    const updated = await addClick(params.prefix , ReferralType.REDIRECT, prisma)
+    
+    return redirect(`${updated.link}?${new URLSearchParams(updated.utmProps as Record<string, string>).toString()}`)
+
+
+}
