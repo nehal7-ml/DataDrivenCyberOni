7a23f921a006a7bce2f1ae193881171ad35cad14
nehal7-ml <nehal.sk.99@gmail.com>
2024-01-15 19:38:09 +0530

    added case studies page

--- components/services/ServiceCard.tsx
+++ components/services/ServiceCard.tsx
@@ -15,7 +15,7 @@ export type ServiceCardProps = {
 
 function ServiceCard({ id, image, previewContent, title }: ServiceCardProps) {
     return (<>
-        <div className="flex flex-col justify-around rounded-xl shadow-lg hover:shadow-lg dark:shadow-[2px_3px_10px_0_#09090b] darkh:hover:shadow-[2px_3px_40px_0_#09090b] dark:bg-zinc-900 p-5 h-full">
+        <div className="flex flex-col justify-around rounded-xl shadow-lg hover:shadow-lg dark:shadow-[2px_3px_10px_0_#09090b] dark:hover:shadow-[2px_3px_40px_0_#09090b] dark:bg-zinc-900 p-5 h-full">
             <Image className="rounded-full h-10 w-10 mb-5"  src={image? image.src :"https://picsum.photos/200?random=1"} alt={title} height={30} width={30} />
             <div className="border-l-4 border-service-green px-3 mb-5 font-bold text-xl">{title}</div>
             <div className="mb-5 line-clamp-3">{previewContent}</div>
