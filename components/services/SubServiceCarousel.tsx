4034e3c5a08ae89b4cc2ee5261d735d745f06dd1
nehal7-ml <nehal.sk.99@gmail.com>
2024-01-25 02:07:41 +0530

    sm:text-center in subservice card

--- components/services/SubServiceCarousel.tsx
+++ components/services/SubServiceCarousel.tsx
@@ -38,10 +38,10 @@ function SubServiceCarousel({ subservices }: { subservices: SubService[] }) {
                 <div className="relative flex flex-row gap-10 p-5 lg:px-10 justify-start w-fit">
                     {subservices.map((subservice, index) =>
                         <div id={subservice.title} key={index} className="flex flex-col w-[80vw] lg:w-[30vw] p-2 lg:p-8 gap-3 rounded-xl bg-gray-100 dark:bg-gray-800 border-4 border-[#AAC3F5]  text-center justify-center mt-10 lg:px-10 pb-10 ">
-                            <div className=" w-full text-left h-fit">
+                            <div className=" w-full text-center  lg:text-left h-fit">
                                 <h3 className="text-lg font-semibold">{subservice.title}</h3>
                             </div>
-                            <div className="z-20  text-left">
+                            <div className="z-20 text-center  lg:text-left">
                                 {subservice.description}
                             </div>
                             <Image
@@ -71,9 +71,9 @@ function SubServiceCarousel({ subservices }: { subservices: SubService[] }) {
                 <Modal setShowModal={setShowModal} showModal={showModal} >
                     <div className="rounded-xl w-fit     p-10 relative container mx-auto text bg-gray-50 shadow-lg dark:bg-gray-800 text-black dark:text-gray-50 flex flex-col items-center justify-center">
                         <button className="absolute right-2 top-1 text-red-500" onClick={()=>setShowModal(false)}><X/></button>
-                        <div>Usage Score: {currentDisplay?.serviceUsageScore}</div>
-                        <div>Esitmated time for completion: {currentDisplay?.estimated_hours_times_one_hundred_percent}</div>
-                        <div>Overhead Cost: {currentDisplay?.overheadCost}</div>
+                        <div>Usage Score: {currentDisplay?.serviceUsageScore}/100</div>
+                        <div>Esitmated time for completion: {currentDisplay?.estimated_hours_times_one_hundred_percent} hrs</div>
+                        <div>Overhead Cost: {currentDisplay?.overheadCost} $</div>
                         <div></div>
                     </div>
                 </Modal>
