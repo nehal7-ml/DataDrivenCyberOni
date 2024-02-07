4d961c65ce0f79b81425f8f67b813c6bafa76de9
nehal7-ml <nehal.sk.99@gmail.com>
2024-02-08 02:36:30 +0530

    filp chat button icon

--- components/ConactButton/index.tsx
+++ components/ConactButton/index.tsx
@@ -9,7 +9,7 @@ function ContactButton() {
     return (
         <>
             <button onClick={() => setShow(true)} className="peer fixed shadow-lg flex justify-center items-center bottom-20 right-5 lg:right-5 lg:bottom-20 h-16 w-16 rounded-full hover:shadow-xl hover:transition-all duration-75 z-[100]">
-                <Image className="object-contain" src={'/images/chat-button.png'} alt="chatbutton" width={60} height={60} />
+                <Image className="object-contain transform scale-x-[-1]" src={'/images/chat-button.png'} alt="chatbutton" width={60} height={60} />
             </button>
             <Modal setShowModal={setShow} showModal={show} >
                     <ContactForm onModal={true} setShowModal={setShow} />
