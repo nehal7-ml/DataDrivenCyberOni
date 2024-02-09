503da48e4e1e48e5977d36b44fe6daf8070dd1cb
nehal7-ml <nehal.sk.99@gmail.com>
2024-02-10 03:07:27 +0530

    added redirect loading screen

--- app/affiliate/[prefix]/page.tsx
+++ app/affiliate/[prefix]/page.tsx
@@ -2,12 +2,13 @@ import { prisma } from "@/prisma/prismaClient";
 import { addClick, update } from "@/crud/referral";
 import { ReferralType } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { sleep } from "@/lib/utils";
 
 
 
 export default async function Referral({ params }: { params: { prefix: string } }) {
     const updated = await addClick(params.prefix, ReferralType.AFFILIATE, prisma)
-
+    await sleep(300)
     return redirect(`${updated.link}?${new URLSearchParams(updated.utmProps as Record<string, string>).toString()}`)
 
 
--- app/referrals/[prefix]/page.tsx
+++ app/referrals/[prefix]/page.tsx
@@ -3,11 +3,12 @@ import { addClick, update } from "@/crud/referral";
 import { ReferralType } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { prisma } from "@/prisma/prismaClient";
+import { sleep } from "@/lib/utils";
 
 
 export default async function Referral({ params }: { params: { prefix: string } }) {
     const updated = await addClick(params.prefix , ReferralType.REDIRECT, prisma)
-    
+    await  sleep(2000)    
     return redirect(`${updated.link}?${new URLSearchParams(updated.utmProps as Record<string, string>).toString()}`)
 
 
