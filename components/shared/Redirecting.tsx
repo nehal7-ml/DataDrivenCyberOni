96e1924fb539693e37020a6efb8581dbd247fac2
nehal7-ml <nehal.sk.99@gmail.com>
2024-02-13 18:31:30 +0530

    fix loading icon

--- components/shared/Redirecting.tsx
+++ components/shared/Redirecting.tsx
@@ -2,14 +2,14 @@ import { LoadingDots } from "./icons";
 
 function Redirecting({ message }: { message: string }) {
     return (
-        <div className="grid h-screen place-content-center bg-white px-4 font-nunito">
+        <div className="grid h-screen place-content-center text-gray-900 dark:text-gray-100  bg-white dark:bg-slate-950 px-4 font-nunito">
             <div className="text-center">
                 {/* Lucide's XCircle icon */}
-                <div className="mx-auto h-5 w-full text-red-600 sm:h-64">
+                <div className="mx-auto h-5 w-full  sm:h-64">
                     <LoadingDots />
                 </div>
 
-                <h3 className="w- mt-6 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+                <h3 className="w- mt-6 text-xl font-bold tracking-tight sm:text-2xl">
                     {message}
                 </h3>
 
