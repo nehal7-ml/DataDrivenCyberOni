ab5448cc8c18b3176d4a77f592b384a15f1f307d
nehal7-ml <nehal.sk.99@gmail.com>
2024-03-13 16:04:38 +0530

    added delete instructions

---
+++ app/privacy/delete-facebook-data/page.tsx
@@ -0,0 +1,10 @@
+import DataDeletionInstructions from "@/components/LongFormText/delete-FB-data";
+
+function DeleteFacebookDataPage() {
+    return ( <div className="mx-auto container">
+        <DataDeletionInstructions />
+
+    </div> );
+}
+
+export default DeleteFacebookDataPage;
