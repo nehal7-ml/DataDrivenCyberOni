2ac5b5dbc3e58f8515391fb682a435445cf5bf5b
nehal7-ml <nehal.sk.99@gmail.com>
2024-03-15 18:28:40 +0530

    use alice carousel for infinite scrolling

--- components/home/CompanyCarousel.tsx
+++ components/home/CompanyCarousel.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Image from "next/image";
 import useWindowSize from "@/lib/hooks/use-window-size";
+import { wrappedSlice } from "@/lib/utils";
+import InfiniteScrollLoop from "../shared/InfinteLoopScroll";
 
 export type Company = {
     name: string;
@@ -12,19 +14,16 @@ function CompanyCarousel({ cards }: { cards: Company[] }) {
     const { isMobile } = useWindowSize();
     const carousel = useRef<HTMLDivElement>(null);
     const [isPaused, setIsPaused] = useState(false);
+    const carouselTrack = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         let interval: NodeJS.Timeout;
 
         if (isMobile && !isPaused) {
             interval = setInterval(() => {
-                if (carousel.current) {
-                    const { scrollLeft, scrollWidth, clientWidth } = carousel.current;
-                    if (scrollLeft >= scrollWidth - clientWidth) {
-                        carousel.current.scrollLeft = 0;
-                    } else {
-                        carousel.current.scrollLeft += 5;
-                    }
+                if (carouselTrack.current) {
+                    const { scrollLeft, scrollWidth, clientWidth } = carouselTrack.current;
+
                 }
             }, 24);
         }
@@ -44,25 +43,31 @@ function CompanyCarousel({ cards }: { cards: Company[] }) {
 
     return (
         <>
-            <div
-                ref={carousel}
-                onMouseOver={handlePause}
-                onMouseOut={handleResume}
-                onClick={handlePause}
-                className="relative flex max-w-full overflow-x-auto lg:flex-wrap gap-1 lg:justify-center scrollbar-none items-center object-contain"
-            >
-                {cards.map((card, index) => (
-                    <div key={index} className="flex items-center h-20 w-30 flex-shrink-0 overflow-hidden">
-                        <Image key={index} className="object-scale-down p-5" src={card.image} alt={card.name} width={160} height={100} />
-                    </div>
-                ))}
-                {/* Duplicate items for infinite scrolling */}
-                {isMobile && cards.map((card, index) => (
-                    <div key={index + cards.length} className="flex items-center h-20 w-30 flex-shrink-0 overflow-hidden">
-                        <Image className="object-scale-down p-5" src={card.image} alt={card.name} width={160} height={100} />
-                    </div>
-                ))}
-            </div>
+            {isMobile ?
+                <InfiniteScrollLoop surroundingBackup={4} baseVelocity={-5} >
+                        {cards.map((card, index) => (
+                            <div key={index} className="flex items-center h-20 w-30 flex-shrink-0 overflow-hidden">
+                                <Image key={index} className="object-scale-down p-5" src={card.image} alt={card.name} width={160} height={100} />
+                            </div>
+                        ))}
+                </InfiniteScrollLoop>
+                :
+                <div
+                    ref={carousel}
+                    onMouseOver={handlePause}
+                    onMouseOut={handleResume}
+                    onClick={handlePause}
+                    className="relative flex max-w-full overflow-x-auto lg:flex-wrap gap-1 lg:justify-center scrollbar-none items-center object-contain"
+                >
+                    {cards.map((card, index) => (
+                        <div key={index} className="flex items-center h-20 w-30 flex-shrink-0 overflow-hidden">
+                            <Image key={index} className="object-scale-down p-5" src={card.image} alt={card.name} width={160} height={100} />
+                        </div>
+                    ))}
+
+                </div>
+
+            }
             <div className="text-center">Our Technology is <span className="font-bold">Used</span> and <span className="font-bold">Trusted</span> by the world’s most ambitious teams.</div>
         </>
     );
