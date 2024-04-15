341d1df9332790ada8aa47b35d6c8393d9f76a5a
01100111011011000110100 1011101000110001101101000 <tyrique1.daniel@gmail.com>
2024-04-15 09:06:59 -0600

    fixed display

--- app/gpt-privacy/page.tsx
+++ app/gpt-privacy/page.tsx
@@ -1,1 +1,13 @@
-import gp
+import GPTPrivacyPolicy from "@/components/LongFormText/gpt-privacy";
+
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <GPTPrivacyPolicy />
+    </div>
+  );
+};
+
+export default page;
