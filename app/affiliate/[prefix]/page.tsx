53948a851e3f26239971e18bc1197f354613c653
nehal7-ml <nehal.sk.99@gmail.com>
2024-04-22 18:58:16 +0530

    fix redirect error and added fallback

--- app/affiliate/[prefix]/page.tsx
+++ app/affiliate/[prefix]/page.tsx
@@ -8,8 +8,14 @@ import { sleep } from "@/lib/utils";
 
 export default async function Referral({ params }: { params: { prefix: string } }) {
     const updated = await addClick(params.prefix, ReferralType.AFFILIATE, prisma)
-    await sleep(300)
-    return redirect(`${updated.link}?${new URLSearchParams(updated.utmProps as Record<string, string>).toString()}`)
+    try {
+        await sleep(2000)
+        const res = await fetch(updated.link);
+        if (res.status >= 400) redirect(updated.fallback);
+    } catch (error) {
+        console.log("redirect error: ", error);
+        redirect(updated.fallback);
+    }
 
-
+    redirect(`${updated.link}?${new URLSearchParams(updated.utmProps as Record<string, string>).toString()}`)
 }
