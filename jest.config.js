467d22ff106958b7acfaff42f78875272a7eea34
nehal7-ml <nehal.sk.99@gmail.com>
2024-05-29 14:23:03 +0000

    added enterprise tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 const nextJest = require('next/jest')
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -13,17 +15,9 @@ const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-
-    // Handle module aliases
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/context/(.*)$': '<rootDir>/context/$1',
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-    "^@/crud/*": "<rootDir>/crud/$1",
-    "^@/app/*":"<rootDir>/app$1",
-    "^@/lib/*":"<rootDir>/lib$1",
-    "^@/styles/*":"<rootDir>/styles$1"  
-  }
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+    
 }
 module.exports = createJestConfig(config)
