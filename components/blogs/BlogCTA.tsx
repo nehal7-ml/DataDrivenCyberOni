4d56ddfe07a2a9e709c884dea6139a15a29cdf30
nehal7-ml <nehal.sk.99@gmail.com>
2024-06-26 15:05:36 +0000

    fixe CTa look

--- components/blogs/BlogCTA.tsx
+++ components/blogs/BlogCTA.tsx
@@ -10,8 +10,8 @@ interface BlogCTAProps {
 }
 function BlogCTA({ title, subTitle, link, button }: BlogCTAProps) {
   return (
-    <div className="mx-auto flex items-center justify-between gap-2 rounded-lg bg-violet-500 p-5 py-8 text-white mx-2 lg:mx-10">
-      <div className="text-ellipsis flex-grow">
+    <div className="mx-2 flex items-center justify-between gap-2 rounded-lg bg-violet-500 p-5 py-8 text-white lg:mx-10">
+      <div className="flex-grow text-ellipsis">
         <h1 className="line-clamp-1 p-1">
           {title ? title : "Join Us grow Your Business"}
         </h1>
@@ -22,12 +22,16 @@ function BlogCTA({ title, subTitle, link, button }: BlogCTAProps) {
         </p>
       </div>
 
-      <Link
-        href={link ? link : "/contact"}
-        className="line-clamp-1 min-w-16 max-w-md text-ellipsis rounded-full bg-white p-3 text-center  text-sm font-semibold text-black hover:bg-gray-300"
-      >
-        {button ? button : "Get Started"}
-      </Link>
+      <div className="flex h-fit w-1/4 max-w-[25%] items-center justify-end text-ellipsis px-5">
+        <Link
+          href={link ? link : "/contact"}
+          className="line-clamp-1 min-w-16 max-w-md text-ellipsis h-full  rounded-full bg-white px-3 leading-10 text-center  text-sm font-semibold text-black hover:bg-gray-300"
+        >
+          {button
+            ? button
+            : "Get Started"}
+        </Link>
+      </div>
     </div>
   );
 }
